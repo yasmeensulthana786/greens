2e27a51c89c9ac9d9c2a88273e9ccc1cf50091ab
yasmeensulthana786 <yasmeenrashid328@gmail.com>
2022-06-23 15:46:37 +0530

    completed

---
+++ src/org/add/GreensTech.java
@@ -0,0 +1,36 @@
+package org.add;
+
+public class GreensTech {
+	private void greensOmr() {
+		System.out.println("Omr");
+		
+
+	}
+	private void greensAdayar() {
+		System.out.println("Adayar");
+		
+		
+	}
+	private void greensTambaram() {
+		System.out.println("Tambaram");
+		
+	}
+	private void greensVelacherry() {
+		System.out.println("Velacherry");
+		
+	}
+	private void greensAnnaNagar() {
+		System.out.println("AnnaNagar");
+		
+
+	}
+	public static void main(String[] args) {
+		GreensTech gt = new GreensTech();
+		gt.greensOmr();
+		gt.greensAdayar();
+		gt.greensTambaram();
+		gt.greensVelacherry();
+		gt.greensAnnaNagar();
+	}
+
+}
